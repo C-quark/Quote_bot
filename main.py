3b09c9ee20226d7c93cb7bc8ffb1792892904a6b
C-quark <90129611+C-quark@users.noreply.github.com>
2023-11-30 01:26:14 +0300

    Исправлен main_pack
    Исправлен main_pack и добавлен файл с именем главного стикерпака в другие хэндлеры

--- main.py
+++ main.py
@@ -19,13 +19,15 @@ STICKER_WIGHT = 512
 STICKER_HEIGHT = 512
 FONT_HEIGHT_NAME = 30
 FONT_HEIGHT_TEXT = 24
-WIGHT_NAME_MAX = 20
-WIGHT_TEXT_MAX = 24
+WIGHT_NAME_MAX = 16
+WIGHT_TEXT_MAX = 23
 LINE_HEIGHT_NAME = 30
 LINE_HEIGHT_TEXT = 27
 INDENT = 20
 
+
 sticker_packs = []
+
 
 commands = '\n'.join([
         '/q - создать стикер из текста',
@@ -110,7 +112,7 @@ def send_sticker(message):
     draw.rounded_rectangle((AVATAR_WIGHT + INDENT, 0, STICKER_WIGHT, height_rr_name), radius=20, fill='#121212', corners=(20, 20, 0, 0), width=1) #302f2f, 191122, 141226, 000000, 121212, 121212, 0E294B, 23282B, 373252, 1A162A, 003038, 001B2E
     draw.text((AVATAR_WIGHT + INDENT * 2, 60), wrapped_text_fill, fill="#E0FFFF", font=font_text)
     draw.text((AVATAR_WIGHT + INDENT * 2, 10), wrapped_name_fill, fill="#AFEEEE", font=font_name)
-    while image.height > STICKER_HEIGHT:
+    while image.height > STICKER_HEIGHT: #TODO исправить высоту стикера
         image = image.resize((STICKER_WIGHT - (LINE_HEIGHT_TEXT + INDENT + AVATAR_WIGHT), STICKER_HEIGHT - (LINE_HEIGHT_TEXT + INDENT)))
     else:
         image.save('sticker.png')
@@ -123,26 +125,31 @@ def send_sticker(message):
 def add_sticker(message):
     if message.reply_to_message and message.reply_to_message.sticker:
         sticker_id = message.reply_to_message.sticker.file_id
-        bot.add_sticker_to_set(user_id=message.from_user.id, name='Quotes_by_Quote_stick_bot', emojis=['\U00002712'], png_sticker=sticker_id)
-        bot.reply_to(message, 'Стикер добавлен')
+        with open('pack_name.txt', 'r') as name:
+            if os.path.getsize('pack_name.txt') == 0:
+                bot.send_message(message.chat.id, 'Выбери главный стикерпак')
+            else:
+                bot.add_sticker_to_set(user_id=message.from_user.id, name=name, emojis=['\U00002712'], png_sticker=sticker_id)
+                bot.reply_to(message, 'Стикер добавлен')
     else:
         bot.reply_to(message, 'Это не стикер, алло!')
 
 
 @bot.message_handler(commands=['del'])
 def del_sticker(message):
-    sticker_set = bot.get_sticker_set('Quotes_by_Quote_stick_bot')
-    if message.reply_to_message and message.reply_to_message.sticker:
-        sticker_id = message.reply_to_message.sticker.file_id
-        for sticker in sticker_set.stickers:
-            if sticker.file_id == sticker_id:
-                bot.delete_sticker_from_set(sticker=sticker_id)
-                bot.reply_to(message, 'Стикер удален')
-                return
+    with open('pack_name.txt', 'r') as name:
+        sticker_set = bot.get_sticker_set(name)
+        if message.reply_to_message and message.reply_to_message.sticker:
+            sticker_id = message.reply_to_message.sticker.file_id
+            for sticker in sticker_set.stickers:
+                if sticker.file_id == sticker_id:
+                    bot.delete_sticker_from_set(sticker=sticker_id)
+                    bot.reply_to(message, 'Стикер удален')
+                    return
+            else:
+                bot.reply_to(message, 'Данного стикера нет в паке, ты не видишь? Сходи к офтальмологу')
         else:
-            bot.reply_to(message, 'Данного стикера нет в паке, ты не видишь? Сходи к офтальмологу')
-    else:
-        bot.reply_to(message, 'Это не стикер, алло!')
+            bot.reply_to(message, 'Это не стикер, алло!')
 
 
 @bot.message_handler(commands=['packs'])
@@ -166,10 +173,10 @@ def new_name(message):
         pack_name = name + '_by_Quote_stick_bot'
         bot.create_new_sticker_set(user_id=549883953, name=pack_name, title='Цитаты', emojis=['\U00002712'], png_sticker='CAACAgIAAxkBAAECLLllZP3pitZmfcLkPaTqUuDDBQcMpwAClisAAmyCMEgrMpVOWbS9YTME')
         bot.send_message(message.chat.id, 'Стикерпак создан')
-        with open('sticker_packs.txt', 'w') as file:
+        with open('sticker_packs.txt', 'w') as packs:
             link_name = 'https://t.me/addstickers/' + pack_name
             sticker_packs.append(link_name)
-            file.write('\n'.join(sticker_packs))
+            packs.write('\n'.join(sticker_packs))
     except ApiTelegramException as e:
         error_description = str(e)
         if 'sticker set name is already occupied' in error_description.lower():
@@ -192,10 +199,10 @@ def del_name(message):
         pack_name = name + '_by_Quote_stick_bot'
         bot.delete_sticker_set(pack_name)
         bot.send_message(message.chat.id, 'Стикерпак удален')
-        with open('sticker_packs.txt', 'w') as file:
+        with open('sticker_packs.txt', 'w') as packs:
             link_name = 'https://t.me/addstickers/' + pack_name
             sticker_packs.remove(link_name)
-            file.write('\n'.join(sticker_packs))
+            packs.write('\n'.join(sticker_packs))
     except ApiTelegramException:
         bot.send_message(message.chat.id, 'Не можешь правильно написать название стикерпака? У тебя лапки?')
     except Exception:
@@ -216,8 +223,14 @@ def main_pack(message):
 def main_name(message):
     name = message.text
     pack_name = name + '_by_Quote_stick_bot'
-    bot.set_chat_sticker_set(CHATS['CHAT'], pack_name)
-    bot.send_message(message.chat.id, 'Выбран главный стикерпак')
+    link_name = 'https://t.me/addstickers/' + pack_name
+    with open('sticker_packs.txt', 'r') as packs:
+        if link_name in packs:
+            with open('pack_name.txt', 'w') as name:
+                name.write(pack_name)
+                bot.send_message(message.chat.id, 'Выбран главный стикерпак')
+        else:
+            bot.send_message(message.chat.id, 'Четче попадай по буквам, мясной')
 
 
 if __name__ == '__main__':
